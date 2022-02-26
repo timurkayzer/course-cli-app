8f57ce15c1adb0fbdf842f7f93e1fd72d0cd9f11
Timur <timurkayzer@gmail.com>
2022-02-26 20:39:19 +0500

    catch errors etc

---
+++ services/api-service.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { getValue } from "./storage-service.js";
+
+
+export async function getWeather(city) {
+    const token = await getValue('token');
+    if (!token) {
+        throw new Error('Не задан ключ API');
+    }
+    // const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const url = new URL('https://api.openweathermap.org/data/2.5/weather');
+    url.searchParams.append('q', city);
+    url.searchParams.append('appid', token);
+    url.searchParams.append('lang', 'ru');
+    url.searchParams.append('units', 'metric');
+
+
+    const httpRes = await axios.get(url.toString());
+    console.log(httpRes.data);
+
+}
--- weather.js
+++ weather.js
@@ -1,18 +1,21 @@
 #!/usr/bin/env node
 
 import { getArgs } from './helpers/args.js';
+import { getWeather } from './services/api-service.js';
 import { printError, printHelp, printSuccess } from './services/log-service.js';
-import { saveKeyValue } from './services/storage-service.js';
+import { getValue, saveKeyValue } from './services/storage-service.js';
 
 async function initCli() {
     const args = getArgs();
+
+    await getForeCast();
 
     if (args.t) {
         saveToken(args.t);
     }
 
-    if (args.s) {
-
+    if (args.c) {
+        saveCity(args.c);
     }
 
     if (args.h) {
@@ -20,6 +23,25 @@ async function initCli() {
     }
 }
 
+async function getForeCast() {
+    try {
+        const city = await getValue('city');
+        const weather = await getWeather(city);
+        console.log(weather);
+    }
+    catch (e) {
+        if (e.response?.status == 401) {
+            printError('Неверный токен');
+        }
+        else if (e.response?.status == 404) {
+            printError('Город не найден');
+        }
+        else {
+            printError(e.message);
+        }
+    }
+
+}
 
 async function saveToken(token) {
     try {
@@ -29,8 +51,16 @@ async function saveToken(token) {
     catch (e) {
         printError(e.message);
     }
+}
 
-
+async function saveCity(city) {
+    try {
+        await saveKeyValue('city', city);
+        printSuccess('Город сохранен');
+    }
+    catch (e) {
+        printError(e.message);
+    }
 }
 
 
